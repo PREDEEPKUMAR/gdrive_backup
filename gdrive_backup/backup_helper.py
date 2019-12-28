fb8a7b8305439e759bd8f0f99baef6d1eb69a089
predi341 <predi341@gmail.com>
2019-12-28 17:24:27 +0530

    Included Move Operation | Condition Logic slightly changed | Removed Execution code

--- gdrive_backup/backup_helper.py
+++ gdrive_backup/backup_helper.py
@@ -19,9 +19,9 @@ class FileOperation:
         self.user_id = user_id
         self.file_names = list()
 
-    def operate_move_files(self):
+    def operate_copy_files(self):
         if self.__check_security(self.user_id, 'M'):
-            self.__move_files()
+            self.__copy_files()
         else:
             return 'User Denied!'
 
@@ -37,18 +37,23 @@ class FileOperation:
         else:
             return 'User Denied!'
 
+    def operate_move_files(self):
+        self.__move_files()
+
     @staticmethod
     def __check_security(user_id, operation):
         """Basic Security Checks"""
         if operation == 'D' and user_id == 'ADMIN':
             return True
-        elif (operation == 'M' or 'Z') and (user_id == 'USER' or 'ADMIN'):
+        elif (operation in ['M', 'Z']) and (user_id in ['USER', 'ADMIN']):
+            return True
+        elif user_id == 'TEST':
             return True
         else:
             return False
 
-    def __move_files(self):
-        """Copy the Eligible Files from Source to Backup_ToDO Directory.
+    def __copy_files(self):
+        """Copy the Eligible Files from Source to Backup_ToDO or Backup_Done Directory.
         Also it captures the eligible file names"""
         for name, stats in self.files.items():
             self.file_names.append(name)
@@ -66,17 +71,21 @@ class FileOperation:
         for file in self.file_names:
             safe_delete.send2trash(os.path.join(self.destination, file))
 
+    def __move_files(self):
+        """Move all the Files from Backup_ToDO to Backup_Done Directory"""
+        source_loc = self.destination
+        zipped_files = os.listdir(source_loc)
+        # ToDO - HardCode has to be removed
+        destiny = os.path.join(source_loc[:-5], 'Done')
+        for name in zipped_files:
+            self.__run_log__(os.path.join(source_loc, name), destiny)
+            shutil.move(os.path.join(source_loc, name), destiny)
+
     def __str__(self):
-        info = 'Class Description: File Operation class for Move, Zipping and Deleting Files'
+        info = 'Class Description: File Operation class for Move, Copy, Zipping and Deleting Files'
         return info
 
-
-from gdrive_backup import search_files as search, identify_files as idf
-SearchTest = search.SearchFiles(search.PC_BACKUP_FOLDER, search.FILE_TYPES)
-files = SearchTest.search_files()
-IDFTest = idf.IdentifyFiles(files, 5)
-Idffiles = IDFTest.identify_files()
-FileTest = FileOperation(Idffiles, PC_DESTINATION, PC_Zip_FILE, 'USER')
-FileTest.operate_move_files()
-FileTest.operate_zip_files()
-FileTest.operate_delete_files()
+    @staticmethod
+    def __run_log__(source, destiny):
+        info = f'Backing up the file {source} to {destiny}'
+        print(info)
